8dfb829248d59bc39dbd183c6cf3a0bb2bf7f9f9
theycallmeaabie <theycallmeaabie@gmail.com>
2026-06-20 05:43:09 +0530

    04_data_preparation/02 Data Transformation/02 Discretization and Binning.ipynb

--- rough.py
+++ rough.py
@@ -1,11 +1,16 @@
 import random
 import matplotlib as plt
-position = 0
-walk = [position]
-nsteps = 1000
-for _ in range(nsteps):
- step = 1 if random.randint(0, 1) else -1
- position += step
- walk.append(position)
+import numpy as np
 
-plt.plot(walk[:100])
+# position = 0
+# walk = [position]
+# nsteps = 1000
+# for _ in range(nsteps):
+#  step = 1 if random.randint(0, 1) else -1
+#  position += step
+#  walk.append(position)
+
+# plt.plot(walk[:100])
+
+arr = np.random.uniform(size=20)
+print(arr)
